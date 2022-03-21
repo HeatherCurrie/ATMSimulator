c3151fe7109fca70adc0d7f64338b8133b99f9c4
HeatherCurrie <heathercurrie1000@gmail.com>
2022-03-21 19:18:07 +0000

    Functional Keypad

---
+++ ATMWindow.designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace ATMSimulator
+{
+    partial class ATMWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnKey1 = new System.Windows.Forms.Button();
+            this.BtnKey3 = new System.Windows.Forms.Button();
+            this.BtnKey2 = new System.Windows.Forms.Button();
+            this.BtnKey7 = new System.Windows.Forms.Button();
+            this.BtnKey6 = new System.Windows.Forms.Button();
+            this.BtnKey4 = new System.Windows.Forms.Button();
+            this.BtnKey5 = new System.Windows.Forms.Button();
+            this.BtnKey8 = new System.Windows.Forms.Button();
+            this.BtnKey9 = new System.Windows.Forms.Button();
+            this.BtnKey0 = new System.Windows.Forms.Button();
+            this.BtnKeyEnter = new System.Windows.Forms.Button();
+            this.BtnKeyCancel = new System.Windows.Forms.Button();
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.LblInstruction = new System.Windows.Forms.Label();
+            this.LblInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnKey1
+            // 
+            this.BtnKey1.Location = new System.Drawing.Point(140, 407);
+            this.BtnKey1.Name = "BtnKey1";
+            this.BtnKey1.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey1.TabIndex = 0;
+            this.BtnKey1.Text = "1";
+            this.BtnKey1.UseVisualStyleBackColor = true;
+            this.BtnKey1.Click += new System.EventHandler(this.BtnKey1_Click);
+            // 
+            // BtnKey3
+            // 
+            this.BtnKey3.Location = new System.Drawing.Point(284, 407);
+            this.BtnKey3.Name = "BtnKey3";
+            this.BtnKey3.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey3.TabIndex = 1;
+            this.BtnKey3.Text = "3";
+            this.BtnKey3.UseVisualStyleBackColor = true;
+            this.BtnKey3.Click += new System.EventHandler(this.BtnKey3_Click);
+            // 
+            // BtnKey2
+            // 
+            this.BtnKey2.Location = new System.Drawing.Point(212, 407);
+            this.BtnKey2.Name = "BtnKey2";
+            this.BtnKey2.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey2.TabIndex = 2;
+            this.BtnKey2.Text = "2";
+            this.BtnKey2.UseVisualStyleBackColor = true;
+            this.BtnKey2.Click += new System.EventHandler(this.BtnKey2_Click);
+            // 
+            // BtnKey7
+            // 
+            this.BtnKey7.Location = new System.Drawing.Point(140, 556);
+            this.BtnKey7.Name = "BtnKey7";
+            this.BtnKey7.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey7.TabIndex = 3;
+            this.BtnKey7.Text = "7";
+            this.BtnKey7.UseVisualStyleBackColor = true;
+            this.BtnKey7.Click += new System.EventHandler(this.BtnKey7_Click);
+            // 
+            // BtnKey6
+            // 
+            this.BtnKey6.Location = new System.Drawing.Point(284, 482);
+            this.BtnKey6.Name = "BtnKey6";
+            this.BtnKey6.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey6.TabIndex = 4;
+            this.BtnKey6.Text = "6";
+            this.BtnKey6.UseVisualStyleBackColor = true;
+            this.BtnKey6.Click += new System.EventHandler(this.BtnKey6_Click);
+            // 
+            // BtnKey4
+            // 
+            this.BtnKey4.Location = new System.Drawing.Point(140, 482);
+            this.BtnKey4.Name = "BtnKey4";
+            this.BtnKey4.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey4.TabIndex = 5;
+            this.BtnKey4.Text = "4";
+            this.BtnKey4.UseVisualStyleBackColor = true;
+            this.BtnKey4.Click += new System.EventHandler(this.BtnKey4_Click);
+            // 
+            // BtnKey5
+            // 
+            this.BtnKey5.Location = new System.Drawing.Point(212, 482);
+            this.BtnKey5.Name = "BtnKey5";
+            this.BtnKey5.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey5.TabIndex = 6;
+            this.BtnKey5.Text = "5";
+            this.BtnKey5.UseVisualStyleBackColor = true;
+            this.BtnKey5.Click += new System.EventHandler(this.BtnKey5_Click);
+            // 
+            // BtnKey8
+            // 
+            this.BtnKey8.Location = new System.Drawing.Point(212, 556);
+            this.BtnKey8.Name = "BtnKey8";
+            this.BtnKey8.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey8.TabIndex = 7;
+            this.BtnKey8.Text = "8";
+            this.BtnKey8.UseVisualStyleBackColor = true;
+            this.BtnKey8.Click += new System.EventHandler(this.BtnKey8_Click);
+            // 
+            // BtnKey9
+            // 
+            this.BtnKey9.Location = new System.Drawing.Point(284, 556);
+            this.BtnKey9.Name = "BtnKey9";
+            this.BtnKey9.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey9.TabIndex = 8;
+            this.BtnKey9.Text = "9";
+            this.BtnKey9.UseVisualStyleBackColor = true;
+            this.BtnKey9.Click += new System.EventHandler(this.BtnKey9_Click);
+            // 
+            // BtnKey0
+            // 
+            this.BtnKey0.Location = new System.Drawing.Point(212, 628);
+            this.BtnKey0.Name = "BtnKey0";
+            this.BtnKey0.Size = new System.Drawing.Size(50, 50);
+            this.BtnKey0.TabIndex = 9;
+            this.BtnKey0.Text = "0";
+            this.BtnKey0.UseVisualStyleBackColor = true;
+            this.BtnKey0.Click += new System.EventHandler(this.BtnKey0_Click);
+            // 
+            // BtnKeyEnter
+            // 
+            this.BtnKeyEnter.BackColor = System.Drawing.Color.LimeGreen;
+            this.BtnKeyEnter.Location = new System.Drawing.Point(365, 407);
+            this.BtnKeyEnter.Name = "BtnKeyEnter";
+            this.BtnKeyEnter.Size = new System.Drawing.Size(75, 50);
+            this.BtnKeyEnter.TabIndex = 10;
+            this.BtnKeyEnter.Text = "Enter";
+            this.BtnKeyEnter.UseVisualStyleBackColor = false;
+            this.BtnKeyEnter.Click += new System.EventHandler(this.BtnKeyEnter_Click);
+            // 
+            // BtnKeyCancel
+            // 
+            this.BtnKeyCancel.BackColor = System.Drawing.Color.Red;
+            this.BtnKeyCancel.Location = new System.Drawing.Point(365, 482);
+            this.BtnKeyCancel.Name = "BtnKeyCancel";
+            this.BtnKeyCancel.Size = new System.Drawing.Size(75, 50);
+            this.BtnKeyCancel.TabIndex = 11;
+            this.BtnKeyCancel.Text = "Cancel";
+            this.BtnKeyCancel.UseVisualStyleBackColor = false;
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitle.Location = new System.Drawing.Point(310, 25);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(172, 29);
+            this.LblTitle.TabIndex = 14;
+            this.LblTitle.Text = "ATM Simulator";
+            // 
+            // LblInstruction
+            // 
+            this.LblInstruction.AutoSize = true;
+            this.LblInstruction.Location = new System.Drawing.Point(321, 108);
+            this.LblInstruction.Name = "LblInstruction";
+            this.LblInstruction.Size = new System.Drawing.Size(0, 17);
+            this.LblInstruction.TabIndex = 15;
+            // 
+            // LblInput
+            // 
+            this.LblInput.AutoSize = true;
+            this.LblInput.Location = new System.Drawing.Point(140, 354);
+            this.LblInput.Name = "LblInput";
+            this.LblInput.Size = new System.Drawing.Size(0, 17);
+            this.LblInput.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.LblInput);
+            this.Controls.Add(this.LblInstruction);
+            this.Controls.Add(this.LblTitle);
+            this.Controls.Add(this.BtnKeyCancel);
+            this.Controls.Add(this.BtnKeyEnter);
+            this.Controls.Add(this.BtnKey0);
+            this.Controls.Add(this.BtnKey9);
+            this.Controls.Add(this.BtnKey8);
+            this.Controls.Add(this.BtnKey5);
+            this.Controls.Add(this.BtnKey4);
+            this.Controls.Add(this.BtnKey6);
+            this.Controls.Add(this.BtnKey7);
+            this.Controls.Add(this.BtnKey2);
+            this.Controls.Add(this.BtnKey3);
+            this.Controls.Add(this.BtnKey1);
+            this.Name = "Form1";
+            this.Text = "ATM Simulator 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnKey1;
+        private System.Windows.Forms.Button BtnKey3;
+        private System.Windows.Forms.Button BtnKey2;
+        private System.Windows.Forms.Button BtnKey7;
+        private System.Windows.Forms.Button BtnKey6;
+        private System.Windows.Forms.Button BtnKey4;
+        private System.Windows.Forms.Button BtnKey5;
+        private System.Windows.Forms.Button BtnKey8;
+        private System.Windows.Forms.Button BtnKey9;
+        private System.Windows.Forms.Button BtnKey0;
+        private System.Windows.Forms.Button BtnKeyEnter;
+        private System.Windows.Forms.Button BtnKeyCancel;
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.Label LblInstruction;
+        private System.Windows.Forms.Label LblInput;
+    }
+}
+
